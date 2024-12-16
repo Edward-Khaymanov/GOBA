d9620cdcc00a243c69bfae4ccbd157688c26a1a0
Edward-Khaymanov <edwardkhaymanov@gmail.com>
2024-12-16 05:26:14 +0300

    add change color methods for progress bar

--- Assets/_Project/Sources/Common/ProgressBar.cs
+++ Assets/_Project/Sources/Common/ProgressBar.cs
@@ -6,18 +6,25 @@ namespace GOBA
     public class ProgressBar : MonoBehaviour
     {
         [SerializeField] private Image _mask;
+        [SerializeField] private Image _fillImage;
+        [SerializeField] private Image _backImage;
+
+        public void SetFillColor(Color color)
+        {
+            _fillImage.color = color;
+        }
+
+        public void SetBackColor(Color color)
+        {
+            _backImage.color = color;
+        }
 
         public void Fill(float current, float total)
         {
-            total = Mathf.Clamp(total, 0f, int.MaxValue);
+            total = Mathf.Clamp(total, 0f, float.MaxValue);
             current = Mathf.Clamp(current, 0f, total);
 
-            var amount = current / total;
-            if (current == 0f)
-            {
-                amount = 0f;
-            }
-
+            var amount = current == 0f ? 0f : current / total;
             _mask.fillAmount = amount;
         }
     }
