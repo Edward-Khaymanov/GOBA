59ef4a47d36ec56d5a9ab73d60d8c4b9cc00c483
Edward-Khaymanov <edwardkhaymanov@gmail.com>
2024-12-23 20:45:04 +0300

    add IParticle manager methods

--- Assets/_Project/Sources/Core/Infrastructure/IParticleManager.cs
+++ Assets/_Project/Sources/Core/Infrastructure/IParticleManager.cs
@@ -1,6 +1,10 @@
-﻿namespace GOBA.CORE
+﻿using UnityEngine;
+
+namespace GOBA.CORE
 {
     public interface IParticleManager
     {
+        public int CreateParticle(string particleName, Vector3 position);
+        public void DestroyParticle(int particleId);
     }
 }
